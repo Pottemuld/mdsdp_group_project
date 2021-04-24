bf607476bd31ed956e063ab0bb8ec253940fa564
Rasmus Jensen <rasje17@student.sdu.dk>
2021-04-24 15:32:00 +0200

    fixed not sharing dsl on git maby...

---
+++ Jjava/src-gen/carrental/Main.java
@@ -0,0 +1,34 @@
+package carrental;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		WorkflowManager manager = new WorkflowManager();
+		Scanner scan = new Scanner(System.in);
+		String input;
+		boolean run = true;
+		while(run) {
+			System.out.println("Please choose your decired workflow");
+			System.out.println("1. create car");
+			System.out.println("2. crete customer");
+			System.out.println("3. rent car");
+			input = scan.nextLine();
+			switch (input) {
+			case "1": {
+				manager.register_car();
+			}
+			case "2":{
+				manager.register_customer();
+			}
+			case "3":{
+				manager.book_car();
+			}
+			case "quit":{
+				run=false;
+			}
+			default:
+				System.out.println("uption not recogniced, type 'quit' to exit");
+			}
+	} 
+	} 
+}
